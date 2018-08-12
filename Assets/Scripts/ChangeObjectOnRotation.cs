08a06ef915b739e12650e6debb02506aa473eddd
Davon <tie-n-dev@ntymail.com>
2018-08-12 11:17:13 +0200

    Add multitarget and Zebracube

---
+++ Assets/Scripts/ChangeObjectOnRotation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ChangeObjectOnRotation : MonoBehaviour {
+
+    public GameObject cube;
+    public GameObject sphere;
+    public GameObject topFace;
+
+
+    // Use this for initialization
+    void Start () {
+        sphere.SetActive(false);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ((topFace.transform.rotation.eulerAngles.x <= 60f || topFace.transform.rotation.eulerAngles.x >= 300f) 
+            && (topFace.transform.rotation.eulerAngles.z <= 60f || topFace.transform.rotation.eulerAngles.z >= 300f))
+        {
+            Debug.Log("Désactive cube");
+            cube.SetActive(false);
+            sphere.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("Désactive sphère");
+            cube.SetActive(true);
+            sphere.SetActive(false);
+        }
+	}
+
+}
